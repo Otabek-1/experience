6bfdf134b0dba784812388fb2492e434b957cad8
Otabek <vjrwqjrkl>
2023-12-27 10:00:08 +0500

    changed

--- new.py
+++ new.py
@@ -1,1 +1,14 @@
-print("Hello mothafuck")
+def calc():
+    print("0.exit")
+    a=input("=>")
+    if a=="0":
+        return req
+    else:
+        print(eval(a))
+        return calc
+
+
+print("Choose one of this:\n1.Calculate\n2.other\n3.coming soon")
+req=int(input("=>"))
+if req==1:
+    print(calc())
